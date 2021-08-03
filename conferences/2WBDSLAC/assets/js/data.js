92190db9ca92654bdf2ddab01fba67e424c535ec
AJVelezRueda <anavelezrueda@gmail.com>
2021-08-03 18:00:12 -0300

    keyworjds

--- conferences/2WBDSLAC/assets/js/data.js
+++ conferences/2WBDSLAC/assets/js/data.js
@@ -17,42 +17,41 @@ const speakersList = [
 const conservatoryList = [
     { "name": "Mailén García", 'img': 'mailen.jpg', "filiation": "Data Observatory with a Gender Perspective - Argentina" },
     { "name": "Fran De Castro Bubani", 'img': 'Fran_Bubani.JPG', "filiation": "CONICET/CAB/IB - Argentina" },
-    { "name": "Eliade Lima", 'img': 'eliade.jpg', "filiation": "UNIPAMPA - Brazil"}
+    { "name": "Eliade Lima", 'img': 'eliade.jpg', "filiation": "UNIPAMPA - Brazil" }
 ]
 
-const keynotesList =[
+const keynotesList = [
     { "name": "Janet Thorton", 'img': 'janet_thorton_square.jpg', "filiation": "EBI - EMBL, UK" },
     { "name": "Han Sun", 'img': 'Han_sun_squared.jpeg', "filiation": "Leibniz - Forschungsinstitut - Germany" }
 ]
 
 
 const BioinfoDisease = [
-    { "number": "1", "title": "DNA blablabla", 'poster': 'poster.jpg', "video": "https://www.youtube.com/embed/kS9yt9z0Gow" },
-    { "number": "2", "title": "RNA blebleble", 'poster': 'cabeza.png', "video": "youtubedotcom" }
+    { "number": "1", "title": "DNA blablabla", 'poster': 'poster.jpg', "video": "https://www.youtube.com/embed/kS9yt9z0Gow", "keyword": ["structre", 'proteins'] },
+    { "number": "2", "title": "RNA blebleble", 'poster': 'cabeza.png', "video": "youtubedotcom", "keyword": ["DNA", 'genomics'] }
 ];
 
 const Structural = [
-    { "number": "3", "title": "esrgsegergs", 'poster': 'esrgsegesh.png', "video": "youtubedotcom" },
-    { "number": "4", "title": "rttytyiyui", 'poster': 'esrgsegesh.png', "video": "youtubedotcom" }
+    { "number": "3", "title": "esrgsegergs", 'poster': 'esrgsegesh.png', "video": "youtubedotcom", "keyword": ["structre", 'proteins'] },
+    { "number": "4", "title": "rttytyiyui", 'poster': 'esrgsegesh.png', "video": "youtubedotcom", "keyword": ["structre", 'proteins'] }
 ];
 
 const ChemoDrug = [
-    { "number": "5", "title": "luiluhilkuy", 'poster': 'esrgsegesh.png', "video": "youtubedotcom" },
-    { "number": "6", "title": "ilhyuilguyil", 'poster': 'esrgsegesh.png', "video": "youtubedotcom" }
+    { "number": "5", "title": "luiluhilkuy", 'poster': 'esrgsegesh.png', "video": "youtubedotcom", "keyword": ["structre", 'proteins'] },
+    { "number": "6", "title": "ilhyuilguyil", 'poster': 'esrgsegesh.png', "video": "youtubedotcom", "keyword": ["structre", 'proteins'] }
 ];
 
 const GenEvol = [
-    { "number": "7", "title": "bnvcncvncvb", 'poster': 'esrgsegesh.png', "video": "youtubedotcom" },
-    { "number": "8", "title": "vcbncncbvn", 'poster': 'esrgsegesh.png', "video": "youtubedotcom" }
+    { "number": "7", "title": "bnvcncvncvb", 'poster': 'esrgsegesh.png', "video": "youtubedotcom", "keyword": ["structre", 'proteins'] },
+    { "number": "8", "title": "vcbncncbvn", 'poster': 'esrgsegesh.png', "video": "youtubedotcom", "keyword": ["structre", 'proteins'] }
 ];
 
 const MlDataMining = [
-    { "number": "9", "title": "yu8itjfd", 'poster': 'esrgsegesh.png', "video": "youtubedotcom" },
-    { "number": "10", "title": "rdyrxgmgbh", 'poster': 'esrgsegesh.png', "video": "youtubedotcom" }
+    { "number": "9", "title": "yu8itjfd", 'poster': 'esrgsegesh.png', "video": "youtubedotcom", "keyword": ["structre", 'proteins'] },
+    { "number": "10", "title": "rdyrxgmgbh", 'poster': 'esrgsegesh.png', "video": "youtubedotcom", "keyword": ["structre", 'proteins'] }
 ];
 
 const Edu = [
-    { "number": "11", "title": "dfdfd", 'poster': 'esrgsegesh.png', "video": "youtubedotcom" },
-    { "number": "12", "title": "dfdfd", 'poster': 'esrgsegesh.png', "video": "youtubedotcom" }
+    { "number": "11", "title": "dfdfd", 'poster': 'esrgsegesh.png', "video": "youtubedotcom", "keyword": ["structre", 'proteins'] },
+    { "number": "12", "title": "dfdfd", 'poster': 'esrgsegesh.png', "video": "youtubedotcom", "keyword": ["structre", 'proteins'] }
 ];
-
