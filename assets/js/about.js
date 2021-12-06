c7b39076a758c660e8b648dd8c500f465b91eb05
lconteville <70072309+lconteville@users.noreply.github.com>
2021-12-06 16:26:48 -0300

    added advisory team

--- assets/js/about.js
+++ assets/js/about.js
@@ -6,36 +6,46 @@ const Coordinators = [
     { "name": "Liliane Conteville", 'img': 'liliane.png', "filiation": "Embrapa Pecuária Sudeste - São Paulo, Brazil" },
     { "name": "Lucy Jiménez", "img": 'lucy.jpeg', "filiation": "Pontificia Universidad Javeriana, Colombia" },
     { "name": "Mercedes Didier Garnham", 'img': 'mercedes.jpeg', "filiation": "Genetics and Bioinformatics Lab, IIB-UNSAM, Argentina" }
-]
+];
+
+const Advisors = [
+    { "name": "Cristina Marino-Buslje", 'img': 'cristina.png', "filiation": "Fundación Instituto Leloir - CONICET, Argentina" },
+    { "name": "Ivana Feldfeber", 'img': 'ivana.jpg', "filiation": "Observatorio de Datos con Perspectiva de Género, Argentina" }
+];
 
 const Members = [
-    { "name": "Cristina Marino-Buslje", 'img': 'cristina.png', "filiation": "Fundación Instituto Leloir - CONICET, Argentina" },
-    { "name": "Ivana Feldfeber", 'img': 'ivana.jpg', "filiation": "Observatorio de Datos con Perspectiva de Género, Argentina" },
-    { "name": "Augustina Nardo", 'img': 'Agus2.jpg', "filiation": "CIDCA -UNLP, Argentina" },
     { "name": "Celeste Grimolizzi", 'img': 'celeste.jpg', "filiation": "3IA-IIIA CONICET UNSAM, Argentina"},
     { "name": "Lucía Guerrero García", 'img': 'lucia.jpg', "filiation": "GIBBS - UNAL, Colombia" },
     { "name": "Letícia Ferreira Lima", 'img':'leticia.jpg', "filiation":"Fundação Oswaldo Cruz - Rio de Janeiro, Brazil"},
+    { "name": "Augustina Nardo", 'img': 'Agus2.jpg', "filiation": "CIDCA -UNLP, Argentina" },
     { "name": "Thayana Vieira Tavares", 'img':'Thayana.jpg', "filiation":"Universidade Federal de São Carlos(UFSCar) - São Paulo, Brazil"},
     { "name": "Clara Cerqueira Matos", 'img':'Clara.jpg', "filiation":"Bahia, Brazil"},
     { "name": "Sindy Licette Piñero", 'img':'Sindy.jpg', "filiation":"Oxiteno S. A. Industria e Comércio, Brazil"}
 ];
+
 
 function renderWeSection(){
     $weSection.empty();
     const TitleDivCoordinators = document.createElement('div');
     const CoordinatorsTitle = document.createElement('h2');
     const CoordinatorsList = document.createElement('ul')
+    const TitleDivAdvisors = document.createElement('div');
+    const AdvisorsTitle = document.createElement('h2');
+    const AdvisorsList = document.createElement('ul');
     const TitleDivMembers = document.createElement('div');
     const MembersTitle = document.createElement('h2');
     const MembersList = document.createElement('ul');
     const span1 = document.createElement('span');
     const span2 = document.createElement('span');
+    const span3 = document.createElement('span');
 
     TitleDivCoordinators.className = "title-div"
     TitleDivMembers.className = "title-div"
+    TitleDivAdvisors.className = "title-div"
 
     span1.className = "underline"
     span2.className = "underline"
+    span3.className = "underline"
     
     CoordinatorsTitle.innerHTML = "General Coordinators";
     CoordinatorsTitle.id ="coordinators-title";
@@ -64,7 +74,6 @@ function renderWeSection(){
         li.appendChild(figure);
         CoordinatorsList.appendChild(li);
     });
-    
 
     MembersTitle.innerHTML = "Members";
     MembersTitle.id ="members-title";
@@ -94,6 +103,34 @@ function renderWeSection(){
         MembersList.appendChild(li);
     });
 
+    AdvisorsTitle.innerHTML = "Advisory Team";
+    AdvisorsTitle.id ="advisors-title";
+    AdvisorsList.className = "memberslist";
+    
+    Advisors.forEach(element => {
+        const li = document.createElement('li');
+        const figure =  document.createElement('figure');
+        const img =  document.createElement('img');
+        const figcappurple =  document.createElement('figcapture');
+        const figcapgray =  document.createElement('figcapture');
+        
+        figure.className = "member";
+        figcappurple.className = "name";
+        
+        img.src = "assets/img/" + element.img;
+        img.alt = element.name + "-img";
+        figcappurple.innerHTML = element.name;
+        figcapgray.innerHTML = element.filiation;
+        
+        
+        figure.appendChild(img);
+        figure.appendChild(figcappurple);
+        figure.appendChild(figcapgray);
+        
+        li.appendChild(figure);
+        AdvisorsList.appendChild(li);
+    });
+
     TitleDivCoordinators.appendChild(CoordinatorsTitle);
     TitleDivCoordinators.appendChild(span1);
 
@@ -104,7 +141,13 @@ function renderWeSection(){
     TitleDivMembers.appendChild(span2);
 
     weSection.appendChild(TitleDivMembers);
-    weSection.appendChild(MembersList);  
+    weSection.appendChild(MembersList);
+
+    TitleDivAdvisors.appendChild(AdvisorsTitle);
+    TitleDivAdvisors.appendChild(span3);
+
+    weSection.appendChild(TitleDivAdvisors);
+    weSection.appendChild(AdvisorsList);
 }
 
 function renderPortugueseAbout() {  
