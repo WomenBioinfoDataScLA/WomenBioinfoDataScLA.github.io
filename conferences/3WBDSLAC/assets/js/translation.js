d093371b868c372143f82157e1362bb53f3c0f5b
lconteville <70072309+lconteville@users.noreply.github.com>
2022-04-25 17:11:42 -0300

    added flags, translated navbar, added new speaker

---
+++ conferences/3WBDSLAC/assets/js/translation.js
@@ -0,0 +1,44 @@
+//Buttons//
+const codeButton = $("#coc-nav-button");
+const speakersButton = $("#speakers-nav-button");
+const programButton = $('#program-nav-button');
+const sponsorsButton = $('#sponsors-nav-button');
+const commiteesButton = $('#comittees-nav-button');
+const spanishButton = document.getElementById('spanish-button');
+const portugueseButton = document.getElementById('portuguese-button');
+
+//Text//
+
+const h1Title = $('#title-h1')
+
+function renderPortuguese() {
+    codeButton.text('Código de Conduta');
+    speakersButton.text('Palestrantes');
+    programButton.text('Programa');
+    sponsorsButton.text('Patrocinadores');
+    commiteesButton.text('Organizadoras');
+    h1Title.text('3º Congresso de Mulheres em Bioinformática & Ciência de Dados LA');
+    h1Title.addClass('h1-pt');
+    h1Title.removeClass('h1-en');
+    h1Title.removeClass('h1-es');
+}
+
+function renderSpanish() {
+    codeButton.text('Código de conducta');
+    speakersButton.text('Oradoras');
+    programButton.text('Programa');
+    sponsorsButton.text('Patrocinadores');
+    commiteesButton.text('Organizadoras');
+    h1Title.text("3ro Congreso de Mujeres en Bioinformática & Ciencia de Datos LA");
+    h1Title.addClass('h1-es');
+    h1Title.removeClass('h1-en');
+    h1Title.removeClass('h1-pt');
+}
+
+spanishButton.addEventListener('click', () => {
+    renderSpanish();
+});
+
+portugueseButton.addEventListener('click', () => {
+    renderPortuguese();
+});
