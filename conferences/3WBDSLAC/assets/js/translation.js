bdcf64418b1c6230713e040f4a833c97a0df32e5
lconteville <70072309+lconteville@users.noreply.github.com>
2022-05-30 18:53:04 -0300

    translations added

--- conferences/3WBDSLAC/assets/js/translation.js
+++ conferences/3WBDSLAC/assets/js/translation.js
@@ -24,7 +24,15 @@ const pSession = $('#p-session')
 const pSync = $('#p-sync')
 const pWorkshops = $('#p-workshops')
 
+//Key Dates/
+const h2Dates = $('#dates-h2')
+const callAbs = $('#call-abs')
+const deadAbs = $('#dead-abs')
+const acceptNot = $('#accept-not')
+const deadVideo = $('#dead-video')
+const deadRegistration = $('#dead-registration')
 
+const h2Sponsors = $('#sponsors-h2')
 
 function renderPortuguese() {
     speakersButton.text('Palestrantes');
@@ -47,13 +55,22 @@ function renderPortuguese() {
     pSession.text('POSTER');
     pSync.text('CURSOS');
     pWorkshops.text('SINCRÔNICOS');
+
+    h2Dates.text('Datas importantes');
+    callAbs.html('<td><span>&#10003;</span>Abertura da Chamada de Resumos</td> <td>16 de Maio, 2022</td>')
+    deadAbs.html('<td><span>&#10003;</span>Prazo para Submissão de Resumos</td> <td>1º de Julho, 2022</td>');
+    acceptNot.html('<td><span>&#10003;</span>Notificação de Aceite</td> <td>10 de Agosto, 2022</td>');
+    deadVideo.html('<td><span>&#10003;</span>Prazo de Envio do Vídeo</td> <td>10 de Setembro, 2022</td>');
+    deadRegistration.html('<td><span>&#10003;</span>Prazo de Inscrição</td> <td>15 de Setembro, 2022</td>');
+
+    h2Sponsors.text('Patrocinadores');
 }
 
 function renderSpanish() {
     speakersButton.text('Oradoras');
     programButton.text('Programa');
     registrationButton.text('Inscripción');
-    submissionButton.text('Sumisión');
+    submissionButton.text('ENVÍOS');
     sponsorsButton.text('Patrocinio');
     commiteesButton.text('Organizadoras');
     h1Title.text("3ro Congreso de Mujeres en Bioinformática & Ciencia de Datos LA");
@@ -64,12 +81,21 @@ function renderSpanish() {
     pMonth.text('SEPTIEMBRE');
     pRegistration.text('GRÁTIS');
     pFree.text('INSCRIPCIÓN');
-    pAbstract.text('SUMISIÓN');
+    pAbstract.text('ENVÍOS');
     pSubmission.text('RESÚMENES');
     pPoster.text('SESIÓN');
     pSession.text('POSTER');
     pSync.text('CURSOS');
     pWorkshops.text('SINCRONICOS');
+    
+    h2Dates.text('Fechas Clave');
+    callAbs.html('<td><span>&#10003;</span>Apertura Convocatoria de Resúmenes</td> <td>16 de Mayo, 2022</td>')
+    deadAbs.html('<td><span>&#10003;</span>Plazo de Envío de Resúmenes</td> <td>1 de Julio, 2022</td>');
+    acceptNot.html('<td><span>&#10003;</span>Notificación de Aceptación</td> <td>10 de Agosto, 2022</td>');
+    deadVideo.html('<td><span>&#10003;</span>Plazo de Envío de Video</td> <td>10 de Septiembre, 2022</td>');
+    deadRegistration.html('<td><span>&#10003;</span>Fecha Límite de Registro</td> <td>15 de Septiembre, 2022</td>');
+    h2Sponsors.text('Patrocinadores');
+
 }
 
 spanishButton.addEventListener('click', () => {
