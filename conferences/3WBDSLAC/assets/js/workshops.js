ebbfd6b9c3e60ac16bee6460c339f44b9e72bc12
lconteville <70072309+lconteville@users.noreply.github.com>
2022-07-19 15:40:50 -0300

    new workshops and metadocencia

--- conferences/3WBDSLAC/assets/js/workshops.js
+++ conferences/3WBDSLAC/assets/js/workshops.js
@@ -2,23 +2,35 @@ const dayOneButton = document.getElementById('workshop-registration');
 const mlWorkshop = $('#machine-learning');
 const bmWorkshop = $('#bacterial-metagenomics');
 const ensemblWorkshop = $('#ensembl-workshop');
+const pfamWorkshop = $('#pfam-workshop');
+const atgWorkshop = $('#atg-workshop');
 const workshopTables = $('#workshop-tables');
-
-
 
 function renderWorkshopsSection() {
     const workshopSectionContent = $(`
-        <a id="mlWorkshopFlyer" href="#" target="_blank" class="workshopsflyer disable">
+        <a id="mlWorkshopFlyer" href="#" class="workshopsflyer disable">
             <img src="./assets/img/workshops/ml_workshop.png" alt="machine learning workshop flyer">
         </a>
 
-        <a id='bmWorkshopFlyer' href="#" target="_blank" class="workshopsflyer disable">
+        <a id='bmWorkshopFlyer' href="#" class="workshopsflyer disable">
             <img src="./assets/img/workshops/bm_workshop.png" alt="machine learning workshop flyer">
         </a>
 
-        <a id='ensemblWorkshopFlyer' href="#" target="_blank" class="workshopsflyer disable">
+        <a id='ensemblWorkshopFlyer' href="#" class="workshopsflyer disable">
             <img src="./assets/img/workshops/ensembl_workshop.png" alt="ensembl workshop flyer">
         </a>
+
+        <a id='pfamWorkshopFlyer' href="#" class="workshopsflyer disable">
+            <img src="./assets/img/workshops/interpro_workshop.png" alt="pfam workshop flyer">
+        </a>
+
+        <a id='pfamWorkshopFlyer' href="#" class="workshopsflyer disable">
+            <img src="./assets/img/workshops/interpro_workshop.png" alt="pfam workshop flyer">
+        </a>
+
+        <a id='atgWorkshopFlyer' href="#" class="workshopsflyer disable">
+            <img src="./assets/img/workshops/atg_workshop.png" alt="atg workshop flyer">
+        </a>        
     `);
 
 
@@ -27,35 +39,75 @@ function renderWorkshopsSection() {
     const mlWorkshopFlyer = $('#mlWorkshopFlyer');
     const bmWorkshopFlyer = $('#bmWorkshopFlyer');
     const ensemblWorkshopFlyer = $('#ensemblWorkshopFlyer');
+    const pfamWorkshopFlyer = $('#pfamWorkshopFlyer');
+    const atgWorkshopFlyer = $('#atgWorkshopFlyer');
 
     mlWorkshop.on('click', () => {
         $(mlWorkshop).addClass('orange-element');
-        $(ensemblWorkshop).removeClass('orange-element');
         $(bmWorkshop).removeClass('orange-element');
+        $(ensemblWorkshop).removeClass('orange-element');
+        $(pfamWorkshop).removeClass('orange-element');
+        $(atgWorkshop).removeClass('orange-element');
         mlWorkshopFlyer.removeClass("disable");
         bmWorkshopFlyer.addClass("disable");
         ensemblWorkshopFlyer.addClass("disable");
+        pfamWorkshopFlyer.addClass("disable");
+        atgWorkshopFlyer.addClass("disable");
     });
 
 
     bmWorkshop.on('click', () => {
+        $(mlWorkshop).removeClass('orange-element');
         $(bmWorkshop).addClass('orange-element');
         $(ensemblWorkshop).removeClass('orange-element');
-        $(mlWorkshop).removeClass('orange-element');
+        $(pfamWorkshop).removeClass('orange-element');
+        $(atgWorkshop).removeClass('orange-element');
         mlWorkshopFlyer.addClass("disable");
-        ensemblWorkshopFlyer.addClass("disable");
         bmWorkshopFlyer.removeClass("disable");
+        ensemblWorkshopFlyer.addClass("disable");
+        pfamWorkshopFlyer.addClass("disable");
+        atgWorkshopFlyer.addClass("disable");
     });    
 
 
     ensemblWorkshop.on('click', () => {
-        $(ensemblWorkshop).addClass('orange-element');
-        $(bmWorkshop).removeClass('orange-element');
         $(mlWorkshop).removeClass('orange-element');
+        $(bmWorkshop).removeClass('orange-element');
+        $(ensemblWorkshop).addClass('orange-element');
+        $(pfamWorkshop).removeClass('orange-element');
+        $(atgWorkshop).removeClass('orange-element');
         mlWorkshopFlyer.addClass("disable");
         bmWorkshopFlyer.addClass("disable");
         ensemblWorkshopFlyer.removeClass("disable");
+        pfamWorkshopFlyer.addClass("disable");
+        atgWorkshopFlyer.addClass("disable");
     });   
+    
+    pfamWorkshop.on('click', () => {
+        $(mlWorkshop).removeClass('orange-element');
+        $(bmWorkshop).removeClass('orange-element');
+        $(ensemblWorkshop).removeClass('orange-element');
+        $(pfamWorkshop).addClass('orange-element');
+        $(atgWorkshop).removeClass('orange-element');
+        mlWorkshopFlyer.addClass("disable");
+        bmWorkshopFlyer.addClass("disable");
+        ensemblWorkshopFlyer.addClass("disable");
+        pfamWorkshopFlyer.removeClass("disable");
+        atgWorkshopFlyer.addClass("disable");
+    }); 
+
+    atgWorkshop.on('click', () => {
+        $(mlWorkshop).removeClass('orange-element');
+        $(bmWorkshop).removeClass('orange-element');
+        $(ensemblWorkshop).removeClass('orange-element');
+        $(pfamWorkshop).removeClass('orange-element');
+        $(atgWorkshop).addClass('orange-element');
+        mlWorkshopFlyer.addClass("disable");
+        bmWorkshopFlyer.addClass("disable");
+        ensemblWorkshopFlyer.addClass("disable");
+        pfamWorkshopFlyer.addClass("disable");
+        atgWorkshopFlyer.removeClass("disable");
+    }); 
 
 }
 
